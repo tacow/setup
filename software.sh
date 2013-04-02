5864358e68f4335e0e89a52d01fe3a5353050e85
yintao <yintao@yintao-centos64.autonavi.com>
2013-04-02 23:09:05 +0800

    Add git & software install shell script

---
+++ software.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+yum install -y git
+yum install -y subversion
+yum install -y vim-enhanced
+yum install -y man
+yum install -y gcc-c++
+yum install -y gdb
+yum install -y valgrind
+yum install -y strace
+yum install -y tree
+yum install -y sysstat
+
