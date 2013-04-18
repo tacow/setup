8abf74aa03516d995db6c465e80ff2aa452e9dfe
yintao <yintao@bogon.autonavi.com>
2013-04-18 16:52:29 +0800

    Add software_ubuntu.sh

---
+++ software_ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt-get install -y git
+sudo apt-get install -y subversion
+sudo apt-get install -y make
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y gdb
+sudo apt-get install -y valgrind
+sudo apt-get install -y systemtap
+sudo apt-get install -y tree
+sudo apt-get install -y sysstat
+
