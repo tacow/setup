6fd5ac5951afbd85709957ca25e1915311988594
yintao <yintao@bogon.autonavi.com>
2013-04-18 17:18:59 +0800

    Add setup_ubuntu.sh

---
+++ setup_ubuntu.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+SCRIPT_PATH=~/setup
+
+# Setup shell
+cat >> ~/.bashrc << EOF
+export LANG=zh_CN.GB18030
+export LANGUAGE=zh_CN.GB18030
+export PATH=\${PATH}:/usr/local/sbin:/usr/sbin:/sbin:${SCRIPT_PATH}/utils
+export PS1='[\u@\h \W]\$ '
+export PS2='> '
+export PS4='+ '
+alias ls='ls -F --color=tty'
+alias ll='ls -lF --color=tty'
+alias lh='ls -lhF --color=tty'
+alias df='df -h'
+alias du='du -sh *'
+alias hist=history
+alias Ps='ps -eo cmd,pid,user,stime,etime'
+EOF
+
+# Setup VIM
+mkdir -p ~/.vim/plugin
+cp -f ${SCRIPT_PATH}/vim/a.vim ~/.vim/plugin
+cp -f ${SCRIPT_PATH}/vim/vimrc ~/.vimrc
+
+# Setup top
+cp -f ${SCRIPT_PATH}/top/toprc ~/.toprc
+
+# Setup systemtap
+mkdir -p ~/stap
+cp -f ${SCRIPT_PATH}/stap/* ~/stap
+
+rm -f ~/setup.tgz
+
+# Setup locale
+sudo sh -c "echo \"zh_CN.GB18030 GB18030\" >> /var/lib/locales/supported.d/zh"
+sudo sh -c "echo \"zh_CN.GBK GBK\" >> /var/lib/locales/supported.d/zh"
+sudo sh -c "echo \"zh_CN.GB2312 GB2312\" >> /var/lib/locales/supported.d/zh"
+sudo locale-gen
+
