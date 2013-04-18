103bc8cc8feeab24528c3177d06ad0c5d47d0642
Yin Tao <taco@163.com>
2013-04-18 15:44:25 +0800

    software.sh

--- software.sh
+++ software.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-yum install -y git
-yum install -y subversion
-yum install -y vim-enhanced
-yum install -y man
-yum install -y make
-yum install -y gcc-c++
-yum install -y gdb
-yum install -y valgrind
-yum install -y strace
-yum install -y ltrace
-yum install -y systemtap
-yum install -y tree
-yum install -y sysstat
-yum install -y yum-utils
+sudo yum install -y git
+sudo yum install -y subversion
+sudo yum install -y vim-enhanced
+sudo yum install -y man
+sudo yum install -y make
+sudo yum install -y gcc-c++
+sudo yum install -y gdb
+sudo yum install -y valgrind
+sudo yum install -y strace
+sudo yum install -y ltrace
+sudo yum install -y systemtap
+sudo yum install -y tree
+sudo yum install -y sysstat
+sudo yum install -y yum-utils
 
