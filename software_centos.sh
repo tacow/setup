9ef3d290d228161ea49a858fe7cdfc35af89bfb8
yintao <yintao@localhost.localdomain>
2013-12-28 20:44:22 -0800

    lynx

--- software_centos.sh
+++ software_centos.sh
@@ -3,6 +3,7 @@ sudo yum install -y git
 sudo yum install -y subversion
 sudo yum install -y vim-enhanced
 sudo yum install -y wget
+sudo yum install -y lynx
 sudo yum install -y man
 sudo yum install -y man-pages
 sudo yum install -y make
