43d546f135ab095517240cb4b09599133b4310c2
yintao <yintao@localhost.localdomain>
2014-02-14 17:59:22 +0800

    MergeArs

--- software_centos.sh
+++ software_centos.sh
@@ -28,6 +28,7 @@ sudo yum install -y java-openjdk
 sudo yum install -y lsof
 sudo yum install -y httpd-tools
 sudo yum install -y nc
+sudo yum install -y ghostscript
 
 sudo yum install -y boost-devel
 sudo yum install -y zlib-devel
