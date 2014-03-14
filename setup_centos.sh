87ba1e3e7c610f44f7137eb934ac102feec27285
Yin Tao <taco@163.com>
2014-03-14 19:01:06 +0800

    sudoers

--- setup_centos.sh
+++ setup_centos.sh
@@ -3,7 +3,7 @@
 # Setup sudoers
 MY_NAME=$(whoami)
 cat >> /tmp/sudoers.append << EOF
-${MY_NAME} ALL=(ALL) ALL
+${MY_NAME} ALL=(ALL) NOPASSWD: ALL
 EOF
 su -c "cat /tmp/sudoers.append >> /etc/sudoers"
 rm -f /tmp/sudoers.append
