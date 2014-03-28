0a25f14061600a50400dcfcea985108940971061
Yin Tao <taco@163.com>
2014-03-28 12:37:14 +0800

    software_ubuntu.sh

--- software_ubuntu.sh
+++ software_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get install -y git
 sudo apt-get install -y subversion
+sudo apt-get install -y lynx-cur
 sudo apt-get install -y make
 sudo apt-get install -y gcc
 sudo apt-get install -y g++
@@ -10,10 +11,16 @@ sudo apt-get install -y valgrind
 sudo apt-get install -y systemtap
 sudo apt-get install -y tree
 sudo apt-get install -y sysstat
+sudo apt-get install -y dstat
+sudo apt-get install -y exuberant-ctags
+sudo apt-get install -y cscope
 sudo apt-get install -y ghostscript
 sudo apt-get install -y lrzsz
 sudo apt-get install -y zip
 sudo apt-get install -y unzip
 sudo apt-get install -y vim
 sudo apt-get install -y openssh-server
+sudo apt-get install -y apache2-utils
+sudo apt-get install -y graphviz
 
+sudo apt-get install -y libboost-all-dev
