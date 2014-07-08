8eb9ed44591689119eca52cd2c2febfcd4fbe2dc
Your Name <you@example.com>
2014-07-08 18:04:24 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 
-IsOS() {
-    n=$(grep -i $1 /etc/issue | wc -l)
-    if [ $n -ge 1 ]
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
 SCRIPT_DIR=$(dirname $0)
+
+echo "OS type[1]:"
+echo "1: Redhat"
+echo "2: Ubuntu"
+read osType
+if [ -z $osType ]
+then
+    osType=1
+fi
 
 sh ${SCRIPT_DIR}/setup_common.sh
 
-if ( IsOS centos || IsOS fedora )
+if [ $osType -eq 1 ]
 then
     sh ${SCRIPT_DIR}/setup_centos.sh
 fi
 
-if IsOS ubuntu
+if [ $osType -eq 2 ]
 then
     sh ${SCRIPT_DIR}/setup_ubuntu.sh
 fi
 
-echo -n "Install softwares now? (y/n) "
-read yn
-if [ $yn = "y" ]
+echo -n "Install softwares now[y]: "
+read install
+if [ -z $install ]
 then
-    if ( IsOS centos || IsOS fedora )
+    install="y"
+fi
+
+if [ $install = "y" ]
+then
+    if [ $osType -eq 1 ]
     then
         sh ${SCRIPT_DIR}/software_centos.sh
     fi
 
-    if IsOS ubuntu
+    if [ $osType -eq 2 ]
     then
         sh ${SCRIPT_DIR}/software_ubuntu.sh
     fi
