af56f42f3df57aecf19d8377fa67463307b54f2b
Your Name <you@example.com>
2014-09-10 11:10:53 +0800

    cmake

--- software_centos.sh
+++ software_centos.sh
@@ -8,7 +8,6 @@ sudo yum install -y lynx
 sudo yum install -y man
 sudo yum install -y man-pages
 sudo yum install -y make
-sudo yum install -y cmake
 sudo yum install -y gcc-c++
 sudo yum install -y gdb
 sudo yum install -y valgrind
