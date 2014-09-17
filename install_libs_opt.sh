cbc9e30a9038c13ba373ba03f4fb42a36918caca
yintao <yintao@localhost.localdomain>
2014-09-17 14:55:52 +0800

    activemq

--- install_libs_opt.sh
+++ install_libs_opt.sh
@@ -12,3 +12,21 @@ cd lib
 make -j 2
 sudo make install
 sudo ldconfig
+
+cd ${SOFTWARE_PATH}
+Wget 'http://mirror.bit.edu.cn/apache//apr/apr-1.5.1.tar.gz'
+tar xzvf apr-1.5.1.tar.gz
+cd apr-1.5.1
+./configure --prefix=/usr/local/
+make -j 2
+sudo make install
+sudo ldconfig
+
+cd ${SOFTWARE_PATH}
+Wget 'http://mirror.bit.edu.cn/apache/activemq/activemq-cpp/3.8.3/activemq-cpp-library-3.8.3-src.tar.gz'
+tar xzvf activemq-cpp-library-3.8.3-src.tar.gz
+cd activemq-cpp-library-3.8.3
+./configure --prefix=/usr/local/
+make -j 2
+sudo make install
+sudo ldconfig
