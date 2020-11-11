fea6e164a4e647bf3d3adf2037aae782296088a4
yintao <tacow@qq.com>
2020-11-11 21:05:55 +0800

    setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -11,7 +11,9 @@ su -c "cat /tmp/sudoers.append >> /etc/sudoers"
 rm -f /tmp/sudoers.append
 
 # Setup locale
+sudo mkdir -p /var/lib/locales/supported.d/
 sudo sh -c "echo \"zh_CN.GB18030 GB18030\" >> /var/lib/locales/supported.d/zh"
 sudo sh -c "echo \"zh_CN.GBK GBK\" >> /var/lib/locales/supported.d/zh"
 sudo sh -c "echo \"zh_CN.GB2312 GB2312\" >> /var/lib/locales/supported.d/zh"
+sudo sh -c "echo \"zh_CN.UTF-8 UTF-8\" >> /var/lib/locales/supported.d/zh"
 sudo locale-gen
